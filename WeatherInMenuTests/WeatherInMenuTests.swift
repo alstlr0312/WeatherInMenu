9fc9b4eb6be818fe8cf8a86540b1db1d057c2721
alstlr <alstlr0312@naver.com>
2025-04-12 14:16:33 +0900

    Initial Commit

---
+++ WeatherInMenuTests/WeatherInMenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherInMenuTests.swift
+//  WeatherInMenuTests
+//
+//  Created by 민식 on 4/12/25.
+//
+
+import Testing
+@testable import WeatherInMenu
+
+struct WeatherInMenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
